bf8913a9774a1f1dd7ab7bd33b1b75a152f68669
Roman Likhachev <ralihachev@gmail.com>
2020-01-28 15:41:58 +0200

    commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/AngRoutePlanner'));
 
 app.all('*', function (req, res)  {
-    res.status(200).sendFile(__dirname + '/dist/index.html');
+    res.status(200).sendFile(__dirname + '/dist/AngRoutePlanner/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
