1bd1606e3247da9f41cb8cfe0ffb5160865b43da
Roman Likhachev <ralihachev@gmail.com>
2020-01-28 15:31:41 +0200

    commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+app.use(express.static(__dirname + '/dist'));
+
+app.all('*', function (req, res)  {
+    res.status(200).sendFile(__dirname + '/dist/index.html');
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 8080);
